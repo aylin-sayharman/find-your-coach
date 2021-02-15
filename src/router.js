b825b8cf872553cad302346081540045f3f12789
Aylin Sayharman <aylin@boomset.com>
2021-02-15 23:55:42 +0300

    First commit

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches'},
+        { path: '/coaches', component: CoachesList },
+        { path: '/coaches/:id', 
+            component: CoachDetail, 
+            props: true,
+            children: [
+            { path: 'contact', component: ContactCoach }
+        ] },
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestsReceived },
+        { path: '/:notFound(.*)', component: NotFound }
+    ]
+});
+
+export default router;
